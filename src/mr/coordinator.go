7cfba2d36d0951fa91300afdca217c40eb955e2f
evvvince <yansu@bupt.edu.cn>
2023-11-13 15:13:01 +0800

    lab1 finish

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,46 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
 type Coordinator struct {
 	// Your definitions here.
-
+	Files          []string
+	TaskId         int
+	ReduceNum      int
+	MapChan        chan *Task
+	ReduceChan     chan *Task
+	TaskMetaHolder map[int]*TaskMetaInfo
+	Phase          Phase
 }
+
+type TaskMetaInfo struct {
+	TaskPtr   *Task
+	Status    TaskStatus
+	StartTime time.Time
+}
+
+var mu sync.Mutex
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,30 +54,196 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
-	// Your code here.
-
-
+	mu.Lock()
+	defer mu.Unlock()
+	if c.Phase == DonePhase {
+		//fmt.Printf("All tasks are finished,the coordinator will be exit! !")
+		ret = true
+	}
 	return ret
 }
 
-//
+func (c *Coordinator) GenerateTaskId() int {
+	res := c.TaskId
+	c.TaskId++
+	return res
+}
+
+func (c *Coordinator) MakeMapTasks(files []string) {
+
+	for _, file := range files {
+		task := Task{
+			TaskId:    c.GenerateTaskId(),
+			FileName:  file,
+			ReduceNum: c.ReduceNum,
+			TaskType:  MapTask,
+		}
+
+		c.MapChan <- &task
+		c.TaskMetaHolder[task.TaskId] = &TaskMetaInfo{
+			TaskPtr: &task,
+			Status:  WaittingStatus,
+		}
+	}
+	fmt.Println("MakeMapTasks Done")
+}
+
+func (c *Coordinator) MakeReduceTasks() {
+	for i := 0; i < c.ReduceNum; i++ {
+		task := Task{
+			TaskId:    c.GenerateTaskId(),
+			ReduceNum: i,
+			TaskType:  ReduceTask,
+		}
+		c.ReduceChan <- &task
+		c.TaskMetaHolder[task.TaskId] = &TaskMetaInfo{
+			TaskPtr: &task,
+			Status:  WaittingStatus,
+		}
+	}
+	//fmt.Println("MakeReduceTasks Done")
+}
+
+func (c *Coordinator) ReleaseTask(args *TaskArgs, reply *Task) error {
+	mu.Lock()
+	defer mu.Unlock()
+	if c.Phase == MapPhase {
+		if len(c.MapChan) > 0 {
+			*reply = *<-c.MapChan
+			if c.MoveTaskStatusToWorking(reply.TaskId) {
+				//fmt.Printf("Map Task %d is working\n", reply.TaskId)
+			}
+		} else {
+			reply.TaskType = WaitingTask
+			if c.CheckMapTaskStatusAllDone() {
+				c.ToNextPhase()
+				return nil
+			}
+		}
+	} else if c.Phase == ReducePhase {
+		if len(c.ReduceChan) > 0 {
+			*reply = *<-c.ReduceChan
+			if c.MoveTaskStatusToWorking(reply.TaskId) {
+				//fmt.Printf("Reduce Task %d is working\n", reply.ReduceNum)
+			}
+		} else {
+			reply.TaskType = WaitingTask
+			if c.CheckReduceTaskStatusAllDone() {
+				c.ToNextPhase()
+				return nil
+			}
+		}
+	} else {
+		reply.TaskType = ExitTask
+	}
+	return nil
+
+}
+
+func (c *Coordinator) FinishTask(args *Task, reply *Task) error {
+	mu.Lock()
+	defer mu.Unlock()
+	if args.TaskType == MapTask {
+		if c.TaskMetaHolder[args.TaskId].Status == WorkingStatus {
+			c.TaskMetaHolder[args.TaskId].Status = DoneStatus
+			//fmt.Printf("Map Task %d is done\n", args.TaskId)
+		}
+
+	} else if args.TaskType == ReduceTask {
+		if c.TaskMetaHolder[args.TaskId].Status == WorkingStatus {
+			c.TaskMetaHolder[args.TaskId].Status = DoneStatus
+			//fmt.Printf("Reduce Task %d is done\n", args.ReduceNum)
+		}
+	}
+	return nil
+}
+
+func (c *Coordinator) MoveTaskStatusToWorking(taskId int) bool {
+	if c.TaskMetaHolder[taskId].Status == WaittingStatus {
+		c.TaskMetaHolder[taskId].Status = WorkingStatus
+		c.TaskMetaHolder[taskId].StartTime = time.Now()
+		return true
+	}
+	return false
+}
+
+func (c *Coordinator) CheckMapTaskStatusAllDone() bool {
+	for _, taskMeta := range c.TaskMetaHolder {
+		if taskMeta.TaskPtr.TaskType == MapTask {
+			if taskMeta.Status != DoneStatus {
+				return false
+			}
+		} else {
+			continue
+		}
+	}
+	//fmt.Println("All Map tasks are done")
+	return true
+}
+
+func (c *Coordinator) CheckReduceTaskStatusAllDone() bool {
+	for _, taskMeta := range c.TaskMetaHolder {
+		if taskMeta.TaskPtr.TaskType == ReduceTask {
+			if taskMeta.Status != DoneStatus {
+				return false
+			}
+		} else {
+			continue
+		}
+	}
+	//fmt.Println("All Reduce tasks are done")
+	return true
+}
+
+func (c *Coordinator) ToNextPhase() {
+	if c.Phase == MapPhase {
+		c.Phase = ReducePhase
+		c.MakeReduceTasks()
+	} else if c.Phase == ReducePhase {
+		c.Phase = DonePhase
+	}
+}
+
+func (c *Coordinator) DetectCrash() {
+	for {
+		time.Sleep(time.Second)
+		mu.Lock()
+		for taskId, taskMeta := range c.TaskMetaHolder {
+			if taskMeta.Status == WorkingStatus {
+				if time.Since(taskMeta.StartTime) > 30*time.Second {
+					fmt.Printf("Task %d is crashed\n", taskId)
+					taskMeta.Status = WaittingStatus
+					if taskMeta.TaskPtr.TaskType == MapTask {
+						c.MapChan <- taskMeta.TaskPtr
+					} else if taskMeta.TaskPtr.TaskType == ReduceTask {
+						c.ReduceChan <- taskMeta.TaskPtr
+					}
+				}
+			}
+		}
+		mu.Unlock()
+	}
+}
+
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		Files:          files,
+		ReduceNum:      nReduce,
+		MapChan:        make(chan *Task, len(files)),
+		ReduceChan:     make(chan *Task, nReduce),
+		TaskMetaHolder: make(map[int]*TaskMetaInfo, len(files)+nReduce),
+	}
 
-	// Your code here.
-
-
+	c.MakeMapTasks(files)
 	c.server()
+	go c.DetectCrash()
 	return &c
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,196 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"path/filepath"
+	"sort"
+	"time"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+type ByKey []KeyValue
+
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+type Task struct {
+	FileName  string
+	TaskId    int
+	TaskType  TaskType
+	ReduceNum int
+}
+
+type TaskArgs struct {
+}
+
+type TaskType int
+
+type Phase int
+
+type TaskStatus int
+
+const (
+	MapTask     TaskType = 0
+	ReduceTask  TaskType = 1
+	WaitingTask TaskType = 2
+	ExitTask    TaskType = 3
+)
+
+const (
+	MapPhase    Phase = 0
+	ReducePhase Phase = 1
+	DonePhase   Phase = 2
+)
+
+const (
+	WorkingStatus  TaskStatus = 0
+	WaittingStatus TaskStatus = 1
+	DoneStatus     TaskStatus = 2
+)
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
 
-	// Your worker implementation here.
-
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+	keepFlag := true
+	for keepFlag {
+		task := GetTask()
+		switch task.TaskType {
+		case MapTask:
+			{
+				DoMapTask(task, mapf)
+				CallDone(task)
+			}
+		case ReduceTask:
+			{
+				DoReduceTask(task, reducef)
+				CallDone(task)
+			}
+		case WaitingTask:
+			//fmt.Println("Waiting for task")
+			time.Sleep(time.Second)
+		case ExitTask:
+			//fmt.Println("All task done")
+			keepFlag = false
+		}
+	}
 
 }
 
-//
+func GetTask() Task {
+	args := TaskArgs{}
+	reply := Task{}
+	ok := call("Coordinator.ReleaseTask", &args, &reply)
+	if ok {
+		//fmt.Printf("GetTask success!\n")
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+	return reply
+}
+
+func CallDone(task Task) Task {
+	args := task
+	reply := Task{}
+	ok := call("Coordinator.FinishTask", &args, &reply)
+	if ok {
+		//fmt.Printf("FinishTask success!\n")
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+	return reply
+}
+func DoMapTask(task Task, mapf func(string, string) []KeyValue) {
+	fileName := task.FileName
+	intermediate := []KeyValue{}
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("cannot open %v", fileName)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", fileName)
+	}
+	file.Close()
+	kva := mapf(fileName, string(content))
+	intermediate = append(intermediate, kva...)
+
+	for _, kv := range intermediate {
+		reduceNum := ihash(kv.Key) % task.ReduceNum
+		oname := fmt.Sprintf("mr-tmp-%d-%d", task.TaskId, reduceNum)
+		ofile, _ := os.OpenFile(oname, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		if ofile != nil {
+			fmt.Fprintf(ofile, "%v %v\n", kv.Key, kv.Value)
+			ofile.Close()
+		}
+	}
+
+}
+
+func DoReduceTask(task Task, reducef func(string, []string) string) {
+	intermediate := []KeyValue{}
+	reduceNum := task.ReduceNum
+	fileName := fmt.Sprintf("mr-tmp-*-%d", reduceNum)
+	filePaths, _ := filepath.Glob(fileName)
+	for _, file := range filePaths {
+		file, err := os.Open(file)
+		if err != nil {
+			log.Fatalf("cannot open %v", file)
+		}
+		var kv KeyValue
+		for {
+			_, err := fmt.Fscanf(file, "%s %s\n", &kv.Key, &kv.Value)
+			if err != nil {
+				break
+			}
+			intermediate = append(intermediate, kv)
+		}
+	}
+
+	i := 0
+	sort.Sort(ByKey(intermediate))
+	oname := fmt.Sprintf("mr-out-%d", reduceNum)
+	ofile, _ := os.Create(oname)
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+
+		// this is the correct format for each line of Reduce output.
+		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	ofile.Close()
+
+}
+
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,13 +215,11 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
-	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	//c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
 	c, err := rpc.DialHTTP("unix", sockname)
 	if err != nil {
